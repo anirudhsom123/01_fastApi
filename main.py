001050135200341ebc8f96d5540ccbce423019d4
aniurdhsom123 <anirudhsom007@gmail.com>
2026-02-22 02:08:46 +0530

    first api

---
+++ main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def hello():
+    return {
+        'message' : 'hello world'
+    }
+@app.get("/about")
+def about():
+    return {
+        'message' : 'about page'
+    }
