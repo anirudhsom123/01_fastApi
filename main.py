680be08cdda1d81c2cf975bbcdb68c465af7ecbb
aniurdhsom123 <anirudhsom007@gmail.com>
2026-02-22 12:42:39 +0530

    patient management system get api

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
+# patient management api
 from fastapi import FastAPI
+import json
 
 app = FastAPI()
+
+def load_data():
+    with open('patients.json','r') as f:
+        data=json.load(f)
+        
+    return data
+    
 
 @app.get("/")
 def hello():
     return {
-        'message' : 'hello world'
+        'message' : 'Patient Management system API'
     }
 @app.get("/about")
 def about():
     return {
-        'message' : 'about page'
+        'message' : 'about page for Patient Management system API'
     }
+
+@app.get('/view')
+def view():
+    data=load_data()
+    return data
