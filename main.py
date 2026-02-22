689c6dd1d80ad29fbea0c75d6dfe4328bf1cc8ee
aniurdhsom123 <anirudhsom007@gmail.com>
2026-02-22 17:48:36 +0530

    sort api

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # patient management api
-from fastapi import FastAPI , Path , HTTPException
+from fastapi import FastAPI , Path , HTTPException ,Query
 import json
 
 app = FastAPI()
@@ -27,6 +27,7 @@ def view():
     data=load_data()
     return data
 
+# path param
 @app.get('/patient/{patient_id}')
 def view_patient(patient_id: str = Path(...,description="return patient details from DB",example='P001')):
     # load patients
@@ -36,3 +37,22 @@ def view_patient(patient_id: str = Path(...,description="return patient details 
         return data[patient_id]
     raise HTTPException(status_code=404,detail="Patient not found")
     # return {"error" : "Patient not found"}
+
+# query Parameter
+@app.get('/sort')
+def sorted_patient(sort_by: str =Query(...,description="select out of height , weight and bmi to sort") ,
+                   order: str =Query('asc',description="select from asc for ascending desc for descending")):
+    valid_fields=['height','weight','bmi']
+
+    if sort_by not in valid_fields:
+        raise HTTPException(status_code=404,detail="select from field height , weight and bmi")
+    
+    if order not in ['asc','desc']:
+        raise HTTPException(status_code=404,detail="select from field asc for ascending and desc for descending")
+    
+    data=load_data()
+    sort_order=True if order=='desc' else False
+
+    sorted_data= sorted(data.values(),key=lambda x:x.get(sort_by,0),reverse=sort_order)
+
+    return sorted_data
